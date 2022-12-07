43ae8f350d54d06008ff8d5572122139ef98250c
Shajadul <shajadulrahe@gmail.com>
2022-12-07 23:41:31 +0600

    Feat: Introduction Page

--- src/Introduction/Intro.js
+++ src/Introduction/Intro.js
@@ -1,9 +1,35 @@
 import styles from "./Intro.module.css";
+// import Image from "../img/shajadul.jpg";
 const Intro = () => {
   return (
     <div className={styles.intro}>
-      <div className={styles.introLeft}>left</div>
-      <div className={styles.introRight}>right</div>
+      <div className={styles.introLeft}>
+        <div className={styles.box1}>
+          <h2 className={styles.box1_intro}>Hello, Working on this</h2>
+          <h2 className={styles.box1_name}>Project</h2>
+          <div className={styles.box1_title}>
+            <div className={styles.box1_title_wrapper}>
+              <div className={styles.box1_title_item}>Software Engineer</div>
+              <div className={styles.box1_title_item}>Web Developer</div>
+              <div className={styles.box1_title_item}>Fron-End Developer</div>
+              <div className={styles.box1_title_item}>JavaScript Developer</div>
+            </div>
+          </div>
+          <div className={styles.box1_discription}>
+            I have expertise in JavaScript and its supporting technologies that
+            solve real-world problems through code.
+            {/* I have expertise in
+            JavaScript and its supporting technologies that solve real-world
+            problems through code. Currently, I am interested in MERN Stack and
+            looking for an opportunity as a front-end developer in a thoughtful
+            and friendly place to utilize and improve my skills. */}
+          </div>
+        </div>
+      </div>
+      <div className={styles.introRight}>
+        <div className={styles.intro_image_bg}></div>
+        {/* <img src={Image} alt="Shajadul" className={styles.intro_img} /> */}
+      </div>
     </div>
   );
 };
