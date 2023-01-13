dc3039950a3c7a560da814c306ccc34695a68606
Shajadul <shajadulrahe@gmail.com>
2023-01-13 16:23:38 +0600

    Feat: Add some new project

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -1,36 +1,39 @@
 import Image1 from "./img/Food-Order-App.png";
 import Image2 from "./img/Expense-Tracker.png";
 import Image3 from "./img/Blog-App.png";
-import Image4 from "./img/portfolio.png";
+import Image5 from "./img/youtube.jpg";
+import Image6 from "./img/quote.jpg";
+import Image7 from "./img/to-do-list.png";
 export const projects = [
   {
     id: 1,
+    img: Image5,
+    link: "https://youtube-clone-shajadul.vercel.app/",
+  },
+  {
+    id: 2,
+    img: Image6,
+    link: "https://quotes-app-shajadul.vercel.app/qoutes",
+  },
+
+  {
+    id: 3,
     img: Image1,
     link: "https://house-of-burger.vercel.app/",
   },
   {
-    id: 2,
+    id: 4,
     img: Image2,
     link: "https://cost-tracker-app.vercel.app/",
   },
   {
-    id: 3,
-    img: Image3,
-    link: "https://my-blogs-app.vercel.app/",
-  },
-  {
-    id: 4,
-    img: Image4,
-    link: "https://shajadul-portfolio.vercel.app/",
-  },
-  {
     id: 5,
-    img: "",
-    link: "",
+    img: Image7,
+    link: "https://to-do-app-shajadul.vercel.app/",
   },
   {
     id: 6,
-    img: "",
-    link: "",
+    img: Image3,
+    link: "https://my-blogs-app.vercel.app/",
   },
 ];
