b159c7923dd286ca6496f4a396f06c44d46e925a
Shajadul Karim Bhuiyan <shajadulrahe@gmail.com>
2025-02-02 14:11:50 +0600

    update: add a new project

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -4,40 +4,47 @@ import Image3 from "./img/Blog-App.png";
 import Image5 from "./img/youtube.jpg";
 import Image6 from "./img/quote.jpg";
 import Image7 from "./img/to-do-list.png";
+import Image8 from "./img/ecom.png";
 export const projects = [
   {
     id: 1,
+    img: Image8,
+    link: "https://shop-in-ecommerce.vercel.app/",
+    title: "E-commerce App",
+  },
+  {
+    id: 2,
     img: Image5,
     link: "https://youtube-clone-shajadul.vercel.app/",
     title: "YouTube Clone",
   },
   {
-    id: 2,
+    id: 3,
     img: Image6,
     link: "https://quotes-app-shajadul.vercel.app/qoutes",
     title: "Qoutes App",
   },
 
   {
-    id: 3,
+    id: 4,
     img: Image1,
     link: "https://house-of-burger.vercel.app/",
     title: "Food Order App",
   },
   {
-    id: 4,
+    id: 5,
     img: Image2,
     link: "https://cost-tracker-app.vercel.app/",
     title: "Expense Tracker",
   },
   {
-    id: 5,
+    id: 6,
     img: Image7,
     link: "https://to-do-app-shajadul.vercel.app/",
     title: "To To App",
   },
   {
-    id: 6,
+    id: 7,
     img: Image3,
     link: "https://my-blogs-app.vercel.app/",
     title: "Blog App",
